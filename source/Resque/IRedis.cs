dff21bd4c08eb24efd4d6bc8f971234cdbc70f0e
Scott Brown <scott.brown@secondsupper.com>
2012-06-12 18:45:18 -0400

    Add redis commands

--- source/Resque/IRedis.cs
+++ source/Resque/IRedis.cs
@@ -5,15 +5,20 @@ namespace Resque
 {
     public interface IRedis
     {
-        bool SAdd(string key, string redisId);
         string LPop(string key);
         void Set(string key, string value);
         long RemoveKeys(params string[] keys);
-        long SRemove(string key, params  string[] values);
-        long RPush(string key, string value);
-        Tuple<string, string> BLPop(string[] keys, int timeoutSeconds = 0);
-        IEnumerable<string> SMembers(string key);
         bool Exists(string key);
         string Get(string key);
+
+        bool SAdd(string key, string redisId);
+        long SRemove(string key, params  string[] values);
+        IEnumerable<string> SMembers(string key);
+        long RPush(string key, string value);
+
+        Tuple<string, string> BLPop(string[] keys, int timeoutSeconds = 0);
+
+        Dictionary<string, string> HGetAll(string key);
+        void HSet(string key, string field, string value);
     }
 }
