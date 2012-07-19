e7c31730e3357cb7811184947651ddd63580e714
Paul Duran <paulduran@gmail.com>
2012-07-19 10:15:04 +1000

    split client functionality out from server - reduced dependencies for client

---
+++ source/Resque/ResqueClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+
+namespace Resque
+{
+    public class ResqueClient
+    {
+        private readonly ConcurrentDictionary<string, Queue> _queues = new ConcurrentDictionary<string, Queue>();
+        public IRedis Client { get; set; }
+
+        public ResqueClient(IRedis client)
+        {
+            Client = client;
+        }
+        public void Push(string queue, string job, params string[] args)
+        {
+            GetQueue(queue).Push(new QueuedItem()
+                                     {
+                                         @class = job,
+                                         args = args
+                                     });
+        }        
+        private Queue GetQueue(string name)
+        {
+            return _queues.GetOrAdd(name, n => new Queue(Client, n));
+        }
+    }
+}
